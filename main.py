93273ed62869992807657d1519015f4c5efefcb7
landongw <landon@extantpublishing.com>
2017-10-11 00:31:41 -0500

    Make pythonic

--- main.py
+++ main.py
@@ -1,66 +1,84 @@
-""" A web crawler to find a link path between two articles on wikipedia.org """
+""" A naive web crawler to find the shortest path between two wikipedia articles.
+
+    DISCLAIMER: CURRENTLY ONLY GOES THREE LEVELS DEEP
+    TODO: Make deeper
+
+"""
 
 import time
-start_time = time.time()
 import wikipedia
 import networkx as nx
-from networkx.algorithms import approximation
+# from networkx.algorithms import approximation
 import matplotlib.pyplot as plt
 
-
+START_TIME = time.time()
 START_TERM = "LaunchCode"
 END_TERM = "The Wealth of Nations"
-START_PAGE = wikipedia.page(START_TERM)  # get start page
-START_LINKS = sorted(START_PAGE.links)  # get links from start page
-edge_list = []  # iterable list for edges
-
-G = nx.Graph()  # create the graph # create storage (array or dict) for page nodes, parent nodes, child nodes
-G.add_node(START_TERM)  # create start node
-G.add_nodes_from(START_LINKS)  # create child nodes
-
-# glevel = 1  # counter for level
-
-for link in START_LINKS:  # for each link create new page node and edge
-    new_tuple = (START_TERM, link)
-    edge_list.append(tuple(new_tuple))
-    NEW_PAGE = wikipedia.page(link)  # get start page
-    NEW_LINKS = sorted(NEW_PAGE.links)
-    G.add_nodes_from(NEW_LINKS)  # create child nodes
-    for new_link in NEW_LINKS:
-        G.add_edge(new_link, link)
-G.add_edges_from(edge_list)  # create edges
 
 
-# adj_articles = sorted(G.adjacency_list())
-# sp = dict(nx.all_pairs_shortest_path(G))
-PATH_BOOL = nx.has_path(G, START_TERM, END_TERM)  # True if a path exists
-SHORT_PATH = nx.shortest_path(G, START_TERM, END_TERM)  # Shortest path
-# SIM_PATH = nx.all_simple_paths(G, START_TERM, END_TERM, 5)
+def build_graph(start):
+    """ Builds the wiki graph """
+    #  TODO: make into recursive loop?
+
+    start_page = wikipedia.page(start)  # get start page
+    start_links = sorted(start_page.links)  # get links from start page
+    graph = nx.Graph()  # Instantiate graph
+    graph.add_node(START_TERM)  # create start node
+    graph.add_nodes_from(start_links)  # create child nodes
+    edge_list = []  # iterable list for edges
+
+    for link in start_links:  # for each link create new page node and edge
+        new_tuple = (START_TERM, link)
+        edge_list.append(tuple(new_tuple))
+        new_page = wikipedia.page(link)  # get start page
+        new_links = sorted(new_page.links)
+        graph.add_nodes_from(new_links)  # create child nodes
+        for new_link in new_links:
+            new_tuple2 = (new_link, link)
+            edge_list.append(tuple(new_tuple2))
+    graph.add_edges_from(edge_list)  # create edges
+    return graph
 
 
-# print('ADJACENT ARTICLES: ', adj_articles)
-# print('ALL NODES: ', list(nx.nodes(G)))
-print(PATH_BOOL)
-print('NUMBER OF NODES: ', G.number_of_nodes())
-print('NUMBER OF EDGES: ', G.number_of_edges())
-# print('ALL PATHS: ', len(list(SIM_PATH)))
-print('SHORTEST PATH: ', SHORT_PATH)
+def draw_test_graph():
+    """ Draw a test graph with dummy data """
+    #  TODO: make this into something useful
+
+    graph = nx.petersen_graph()
+
+    plt.subplot(121)
+
+    nx.draw(graph, with_labels=True, font_weight='bold')
+    plt.subplot(122)
+
+    nx.draw_shell(graph,
+                  nlist=[range(5, 10), range(5)],
+                  with_labels=True,
+                  font_weight='bold')
+    plt.show()
 
 
-# #####
-# ######  DRAW THE TEST GRAPH WITH DUMMY DATA
-# #####
+def main():
+    """ Runs this file and prints output """
 
-# G = nx.petersen_graph()
-# plt.subplot(121)
+    graph = build_graph(START_TERM)
+    path_exists = nx.has_path(graph, START_TERM, END_TERM)  # True if exists
+    short_path = nx.shortest_path(graph, START_TERM, END_TERM)  # Shortest path
 
-# nx.draw(G, with_labels=True, font_weight='bold')
-# plt.subplot(122)
+    # adj_articles = sorted(G.adjacency_list())
+    # sp = dict(nx.all_pairs_shortest_path(G))
+    # SIM_PATH = nx.all_simple_paths(G, START_TERM, END_TERM, 5)
+    # print('ADJACENT ARTICLES: ', adj_articles)
+    # print('ALL NODES: ', list(nx.nodes(G)))
+    # print('NUMBER OF NODES MAPPED: ', G.number_of_nodes())
+    # print('NUMBER OF EDGES: ', G.number_of_edges())
+    # print('ALL PATHS: ', len(list(SIM_PATH)))
 
-# nx.draw_shell(G, nlist=[range(5, 10), range(5)], with_labels=True, font_weight='bold')
-# plt.show()
+    if path_exists:
+        print('SHORTEST PATH: ', short_path)
+    else:
+        print('NO PATH FOUND')
+    print("TIME: %s seconds" % (time.time() - START_TIME))
 
-# nx.clustering(G)
-
-print("TIME: %s seconds" % (time.time() - start_time))
-
+if __name__ == "__main__":
+    main()
