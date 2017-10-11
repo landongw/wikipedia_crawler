bd995748e76cb8768275e7e17e0f0cb69afa47c2
landongw <landon@extantpublishing.com>
2017-10-11 18:12:33 -0500

    Hide status printing

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import networkx as nx
 
 START_TIME = time.time()
 START_TERM = "LaunchCode"
-END_TERM = "The Wealth of Nations"
+END_TERM = "Facebook"
 PATH_BOOL = False
 depth = 1
 
@@ -29,9 +29,9 @@ def build_graph(start, stop):
         for child_name in child_name_list:  # add edges between start node and child nodes
             graph.add_edge(child_name, parent_name)
 
-        print('NUMBER OF NODES MAPPED: ', graph.number_of_nodes())
-        print('NUMBER OF EDGES MAPPED: ', graph.number_of_edges())
-        print('DEPTH: ', depth)
+        # print('NUMBER OF NODES MAPPED: ', graph.number_of_nodes())
+        # print('NUMBER OF EDGES MAPPED: ', graph.number_of_edges())
+        # print('DEPTH: ', depth)
         parent_name_list = child_name_list  # set start as list of previous iteration's children
         depth += 1
 
@@ -41,18 +41,18 @@ def build_graph(start, stop):
             try:
                 parent_page = wikipedia.page(parent_name2)  # get parent page
             except wikipedia.exceptions.PageError:  # skips if page doesn't exist
-                print('PageError, continuing.')
+                print('PageError, continuing. Please wait.')
                 continue
             except wikipedia.exceptions.DisambiguationError:  # TODO: find a way to prevent this error
-                print('Disambiguation Error, continuing.')
+                print('Disambiguation Error, continuing.  Please wait.')
                 continue
             child_name_list = parent_page.links  # create list of child nodes
             graph.add_nodes_from(child_name_list)  # add child nodes to graph
             for child_name in child_name_list:  # add edges between start node and child nodes
                 graph.add_edge(child_name, parent_name2)
-            print('NUMBER OF NODES MAPPED: ', graph.number_of_nodes())
-            print('NUMBER OF EDGES MAPPED: ', graph.number_of_edges())
-            print('DEPTH: ', depth)
+            # print('NUMBER OF NODES MAPPED: ', graph.number_of_nodes())
+            # print('NUMBER OF EDGES MAPPED: ', graph.number_of_edges())
+            # print('DEPTH: ', depth)
             if stop in child_name_list:  # tests if path exists
                 PATH_BOOL = True
         parent_name_list = child_name_list  # sets the parent_name_list to the old children before the next loop
@@ -60,7 +60,8 @@ def build_graph(start, stop):
 
 
 def path_exists(graph, start, end):
-    """ Tests if path exists between start and end, returns Bool """
+    """ Tests if path exists between start and end,
+    returns True or some gibberish that breaks my progam """
 
     test = nx.has_path(graph, start, end)
     if test is True:
@@ -73,12 +74,14 @@ def path_exists(graph, start, end):
 def main(parser="lxml"):
     """ Runs this file and prints output, receives parser to try and 
     avoid rare Disambiguation Errors -- which doesn't work  yet 
-    -- handled currenlty by continuing on line 48. """
-
+    -- handled currently by continuing on line 48. """
+    print('Crawling wikipedia, assembling model.  Please wait.')
     graph = build_graph(START_TERM, END_TERM)
 
     if PATH_BOOL:
         short_path = nx.shortest_path(graph, START_TERM, END_TERM)  # Find shortest path
+        print('NUMBER OF NODES MAPPED: ', graph.number_of_nodes())
+        # print('NUMBER OF EDGES MAPPED: ', graph.number_of_edges())
         print('DEGREES: ', depth - 1)
         print('SHORTEST PATH: ', short_path)
     else:
